a9886f9421ec5bc16049e9088e0bccfdb1fda955
Jamaal Sedayao <you@example.com>
2015-08-13 12:40:46 -0400

    First Commit WIP

---
+++ OptionSelector/OSCategoryTBV.h
@@ -0,0 +1,16 @@
+//
+//  OSCategoryTBV.h
+//  OptionSelector
+//
+//  Created by Jamaal Sedayao on 8/13/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CQCategory.h"
+
+@interface OSCategoryTBV : UITableViewController
+
+@property (nonatomic) NSMutableArray * allCategories; // holds all our instances of CQcategory
+
+@end
---
+++ OptionSelector/OSOptionsTBV.m
@@ -0,0 +1,112 @@
+//
+//  OSOptionsTBV.m
+//  OptionSelector
+//
+//  Created by Jamaal Sedayao on 8/13/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "OSOptionsTBV.h"
+
+@interface OSOptionsTBV ()
+
+@end
+
+@implementation OSOptionsTBV
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.optionsCategory.options.count; //returns count of the options array in object selected
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"optionsReuseIdentifier" forIndexPath:indexPath];
+    
+    cell.textLabel.text = [self.optionsCategory.options objectAtIndex:indexPath.row]; //places each string in options array on cells
+    
+    return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    // gives us checkmarks
+    
+    if(self.checkedIndexPath)
+    {
+        UITableViewCell* uncheckCell = [tableView cellForRowAtIndexPath:self.checkedIndexPath];
+        uncheckCell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    UITableViewCell* cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    
+    self.checkedIndexPath = indexPath;
+    
+    // passing data to first array
+    
+    self.optionsCategory.selection = [self.optionsCategory.options objectAtIndex:indexPath.row];
+    
+    
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
