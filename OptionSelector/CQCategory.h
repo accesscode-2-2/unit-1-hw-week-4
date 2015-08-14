29b1c762c34489ab9c08d4cb1693c51f3e9a704c
Jason Wang <qwang216@gmail.com>
2015-08-14 19:34:36 -0400

    working progress

---
+++ OptionSelector/CQCategory.h
@@ -0,0 +1,13 @@
+//
+//  CQCategory.h
+//  OptionSelector
+//
+//  Created by Jason Wang on 8/14/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CQCategory : NSObject
+
+@end
---
+++ OptionSelector/CatagoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CatagoryTableViewController.h
+//  OptionSelector
+//
+//  Created by Jason Wang on 8/14/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CatagoryTableViewController : UITableViewController
+
+@end
