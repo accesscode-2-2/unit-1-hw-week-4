4cdfe677f5586eae4e2777544d72d6c9f447ee06
Bereket Ghebremedhin <bereket6725@gmail.com>
2015-08-14 23:52:09 -0400

    made changes

---
+++ OptionSelector/CQCategoryListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  CQCategoryListTableViewController.h
+//  OptionSelector
+//
+//  Created by Bereket  on 8/14/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CQCategoryListTableViewController : UITableViewController
+
+@property (nonatomic) NSArray *listOfCategories;
+
+
+@end
---
+++ OptionSelector/CQOptionsListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  CQOptionsListTableViewController.h
+//  OptionSelector
+//
+//  Created by Bereket  on 8/14/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CQCategory.h"
+@interface CQOptionsListTableViewController : UITableViewController
+
+@property (nonatomic) CQCategory* someCategory;
+
+@end
