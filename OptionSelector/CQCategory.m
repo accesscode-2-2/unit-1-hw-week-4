226de0c5bc59cd9f20b1bd680346d3fc54962c23
Christian <maldonadoguitar@gmail.com>
2015-09-30 12:47:42 -0400

    update again

--- OptionSelector/CQCategory.m
+++ OptionSelector/CQCategory.m
@@ -10,57 +10,9 @@
 
 @implementation CQCategory
 
-//-(void)intializeData {
-//    
-//    
-//    [self intializeOptions];
-//    [self intializeName];
-//    [self intializeSelection];
-//    
-//}
-//
-//
-//-(void)intializeSelection{
-//    
-//}
-//
-//
-//-(void)intializeName{
-//    
-//}
-//
-//
-//
-//-(void)intializeOptions{
-//    
-//    NSArray *fender = @[
-//                        @"Fender Stratocaster",
-//                        @"Telecaster",
-//                        @"Mustang",
-//                        @"Jaguar",
-//                        @"Jazz Master",
-//                        ];
-//    NSArray *gibson = @[
-//                        @"Gibson Explorer",
-//                        @"Les Paul",
-//                        @"Thunderbird",
-//                        @"SG",
-//                        ];
-//    NSArray *ibanez = @[
-//                        @"Iceman",
-//                        @"Artstar",
-//                        @"Destroyer",
-//                        @"Artcore",
-//                        ];
-//    NSArray *turner = @[
-//                        @"Model 1",
-//                        @"Model T",
-//                        @"Renaissance RN-6h",
-//                        @"Renaissance RS-6",
-//                        ];
-//                        
-//                        
-//                        
-//}
+-(void)intializeData{
+    
+}
+
 
 @end
